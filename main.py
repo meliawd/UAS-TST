f3e10d687149f4df39528eb4e3d1885c36e0d71b
amelia18220043 <74282979+meliawd@users.noreply.github.com>
2022-12-09 22:33:07 +0700

    Update main.py

--- main.py
+++ main.py
@@ -76,4 +76,4 @@ async def predict_kelulusan(data:mahasiswa):
     return {"prediksi" : prediksi[0]}
 
 if __name__ == '__main__':
-    uvicorn.run("main:app", host="0.0.0.0", port=os.getenv("PORT", 8080), reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=int(os.getenv("PORT", 8080)), reload=True)
