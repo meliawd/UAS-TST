b1cb76442c908d18f6b3ce8b80d1c3741feddac1
amelia18220043 <74282979+meliawd@users.noreply.github.com>
2022-12-09 22:12:03 +0700

    update

--- models.py
+++ models.py
@@ -1,14 +1,14 @@
 from database import Base
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, FLOAT
 from sqlalchemy.orm import relationship
 
 class data_Mahasiswa(Base):
     __tablename__="Mahasiswa"
     nim = Column(Integer,primary_key=True)
     nama = Column(String)
-    ip1 = Column(float)
-    ip2 = Column(float)
-    ip3 = Column(float)
-    ip4 = Column(float)
+    ip1 = Column(FLOAT)
+    ip2 = Column(FLOAT)
+    ip3 = Column(FLOAT)
+    ip4 = Column(FLOAT)
     sks = Column(Integer)
     predict_ontime = Column(Boolean)
